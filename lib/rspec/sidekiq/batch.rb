138e18b531d03983f64c5e00d1a5fe7695b8b680
Aaron Daniel <amdtech@me.com>
2015-08-10 16:07:00 -0700

    Add support for rr + warning for unsupported mocks

--- lib/rspec/sidekiq/batch.rb
+++ lib/rspec/sidekiq/batch.rb
@@ -65,9 +65,16 @@ if defined? Sidekiq::Batch
 
       if mocked_with_mocha?
         Sidekiq::Batch.stubs(:new) { RSpec::Sidekiq::NullBatch.new }
-      else
+      elsif supports_allow?
         allow(Sidekiq::Batch).to receive(:new)  { RSpec::Sidekiq::NullBatch.new }
         allow(Sidekiq::Batch::Status).to receive(:new)  { RSpec::Sidekiq::NullStatus.new }
+      elsif supports_stub?
+        stub(Sidekiq::Batch).new { RSpec::Sidekiq::NullBatch.new }
+        stub(Sidekiq::Batch::Status).new { RSpec::Sidekiq::NullStatus.new }
+      else
+        message = '[rspec-sidekiq] The mocking library you\'re using is not supported yet. There will probably be failures.'
+        message = "\e[33m#{message}\e[0m" if RSpec::Sidekiq.configuration.enable_terminal_colours
+        puts message if RSpec::Sidekiq.configuration.warn_unsupported_mocking_library
       end
     end
   end
@@ -75,5 +82,13 @@ if defined? Sidekiq::Batch
   ## Helpers ----------------------------------------------
   def mocked_with_mocha?
     Sidekiq::Batch.respond_to? :stubs
+  end
+
+  def supports_allow?
+    self.respond_to?(:allow) && self.respond_to?(:receive)
+  end
+
+  def supports_stub?
+    self.respond_to?(:stub)
   end
 end
--- lib/rspec/sidekiq/configuration.rb
+++ lib/rspec/sidekiq/configuration.rb
@@ -1,11 +1,15 @@
 module RSpec
   module Sidekiq
     class Configuration
-      attr_accessor :clear_all_enqueued_jobs, :enable_terminal_colours, :warn_when_jobs_not_processed_by_sidekiq
+      attr_accessor :clear_all_enqueued_jobs,
+                    :enable_terminal_colours,
+                    :warn_unsupported_mocking_library,
+                    :warn_when_jobs_not_processed_by_sidekiq
 
       def initialize
         @clear_all_enqueued_jobs = true
         @enable_terminal_colours = true
+        @warn_unsupported_mocking_library = true
         @warn_when_jobs_not_processed_by_sidekiq = true
       end
     end
